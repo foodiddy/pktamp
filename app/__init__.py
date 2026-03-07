ed9693a75178969593de68191c2a04177b090caa
foo <foo@clank.home.arpa>
2026-03-06 22:18:49 -0500

    fix: configure Jinja2 to use Vue.js-style delimiters

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from jinja2 import Environment
 from .api import api_bp
 from .config import logger
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
+    
+    # Configure Jinja2 to use Vue.js-style delimiters
+    app.jinja_env.variable_start_string = '{{{'
+    app.jinja_env.variable_end_string = '}}}'
+    
     app.register_blueprint(api_bp, url_prefix="/api")
     
     @app.route("/")
